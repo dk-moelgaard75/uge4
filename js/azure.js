79861fd046f39697249e9001ad5d98156353e6dd
Kåre Mølgaard Rasmussen <34368915+dk-moelgaard75@users.noreply.github.com>
2020-09-27 18:42:05 +0200

    Add azure funtion to website

---
+++ js/azure.js
@@ -0,0 +1,13 @@
+function getJson() {
+    var url = 'https://kmrgetmessage.azurewebsites.net/api/GetJSON?code=Nf/VkbTQIWDwHdqJ2wQdocwzeRIGPd84A4eKVtvZ6blRBETjHXbaUw=='
+    $.ajax(url,
+        {
+            success: function (data, status, xhr) {// success callback function
+
+                var returnedData = JSON.parse(data);
+                var html = 'Name:' + returnedData.name + ' - location:' + returnedData.location
+
+                $('#azuredata').append(data);
+            }
+        });
+}
